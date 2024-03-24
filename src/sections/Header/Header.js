79717e7e15762c52d14bc3ddf0cc9761fa9f2051
Zinou-lite <mkrym99999@gmail.com>
2024-03-23 20:43:21 -1000

    responsive header

--- src/sections/Header/Header.js
+++ src/sections/Header/Header.js
@@ -1,35 +1,56 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Header.css"
-import menu from "../../assets/menu.svg"
-import close from "../../assets/close.svg"
+import menu from "../../assets/menu.png"
+import close from "../../assets/close.png"
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false)
+
   return (
     <header className="header-section">
       <div className="header-content">
         <span className="header-logo">innovation.</span>
-        <nav className="header-nav">
-          <a href="#hero">Home</a>
-          <a href="#about">About</a>
-          <a href="#courses">Courses</a>
-          <a href="#contact">Contact</a>
+        <nav className="header-nav-desktop">
+          <a className="header-nav-desktop-childs" clas href="#hero">
+            Home
+          </a>
+          <a className="header-nav-desktop-childs" href="#about">
+            About
+          </a>
+          <a className="header-nav-desktop-childs" href="#courses">
+            Courses
+          </a>
+          <a className="header-nav-desktop-childs" href="#contact">
+            Contact
+          </a>
         </nav>
-        {/* <nav>
+        <div className="header-nav-mobile">
           <img
-            src={toggle ? close : menu}
+            src={!toggle ? close : menu}
             alt="menu"
-            className="w-[28px] h-[28px] object-contain"
+            width={29}
+            height={29}
             onClick={() => setToggle(!toggle)}
           />
-          <div>
-            <nav className="header-nav">
-              <a href="#hero">Home</a>
-              <a href="#about">About</a>
-              <a href="#courses">Courses</a>
-              <a href="#contact">Contact</a>
+          <div
+            className={`${toggle ? "hidden" : "header-nav-mobile-container"}`}
+          >
+            <nav className="nav-elements-mobile">
+              <a href="#hero" onClick={() => setToggle(!toggle)}>
+                Home
+              </a>
+              <a href="#about" onClick={() => setToggle(!toggle)}>
+                About
+              </a>
+              <a href="#courses" onClick={() => setToggle(!toggle)}>
+                Courses
+              </a>
+              <a href="#contact" onClick={() => setToggle(!toggle)}>
+                Contact
+              </a>
             </nav>
           </div>
-        </nav> */}
+        </div>
       </div>
     </header>
   )
