06de2c298c851d9c198d998e223433a832a1d6c7
mahfoud-mehalli <mahfoudmehalli12345@gmail.com>
2024-03-24 12:03:20 +0100

    changes on courses | add screenshoot

--- src/sections/Courses/Courses.js
+++ src/sections/Courses/Courses.js
@@ -1,6 +1,11 @@
 import React from "react"
 import "./Courses.css"
-import cardImage from "../../assets/card-image.png"
+import course1 from "../../assets/course1.png"
+import course2 from "../../assets/course2.png"
+import course3 from "../../assets/course3.png"
+import course4 from "../../assets/course4.png"
+import course5 from "../../assets/course5.png"
+import course6 from "../../assets/course6.png"
 
 const Courses = () => {
   return (
@@ -11,10 +16,46 @@ const Courses = () => {
         </div>
         <div className="courses-cards">
           <div className="courses-card">
-            <img src={cardImage} alt="card-image" />
+            <img src={course1} alt="card-image" />
             <div className="courses-infos">
               <h3 className="courses-card-title">
                 Introduction to Web Development
+              </h3>
+              <div>
+                <span>
+                  <p>By Mahfoud Mehalli</p>
+                  <p>Free</p>
+                </span>
+                <span>
+                  <p>LEVEL : Beginner</p>
+                  <button onclick="openModal()">Register</button>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="courses-card">
+            <img src={course2} alt="card-image" />
+            <div className="courses-infos">
+              <h3 className="courses-card-title">
+                  Digital Marketing
+              </h3>
+              <div>
+                <span>
+                  <p>By Mahfoud Mehalli</p>
+                  <p>Free</p>
+                </span>
+                <span>
+                  <p>LEVEL : Intermidiate</p>
+                  <button>Register</button>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="courses-card">
+            <img src={course3} alt="card-image" />
+            <div className="courses-infos">
+              <h3 className="courses-card-title">
+                  Employe Training
               </h3>
               <div>
                 <span>
@@ -29,10 +70,28 @@ const Courses = () => {
             </div>
           </div>
           <div className="courses-card">
-            <img src={cardImage} alt="card-image" />
+            <img src={course4} alt="card-image" />
             <div className="courses-infos">
               <h3 className="courses-card-title">
-                Introduction to Web Development
+                  International Business Managment
+              </h3>
+              <div>
+                <span>
+                  <p>By Mahfoud Mehalli</p>
+                  <p>Free</p>
+                </span>
+                <span>
+                  <p>LEVEL : Intermidiate</p>
+                  <button>Register</button>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="courses-card">
+            <img src={course5} alt="card-image" />
+            <div className="courses-infos">
+              <h3 className="courses-card-title">
+                  Charecter Design
               </h3>
               <div>
                 <span>
@@ -47,10 +106,10 @@ const Courses = () => {
             </div>
           </div>
           <div className="courses-card">
-            <img src={cardImage} alt="card-image" />
+            <img src={course6} alt="card-image" />
             <div className="courses-infos">
               <h3 className="courses-card-title">
-                Introduction to Web Development
+                  Social Psychology
               </h3>
               <div>
                 <span>
@@ -58,61 +117,7 @@ const Courses = () => {
                   <p>Free</p>
                 </span>
                 <span>
-                  <p>LEVEL : Beginner</p>
-                  <button>Register</button>
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="courses-card">
-            <img src={cardImage} alt="card-image" />
-            <div className="courses-infos">
-              <h3 className="courses-card-title">
-                Introduction to Web Development
-              </h3>
-              <div>
-                <span>
-                  <p>By Mahfoud Mehalli</p>
-                  <p>Free</p>
-                </span>
-                <span>
-                  <p>LEVEL : Beginner</p>
-                  <button>Register</button>
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="courses-card">
-            <img src={cardImage} alt="card-image" />
-            <div className="courses-infos">
-              <h3 className="courses-card-title">
-                Introduction to Web Development
-              </h3>
-              <div>
-                <span>
-                  <p>By Mahfoud Mehalli</p>
-                  <p>Free</p>
-                </span>
-                <span>
-                  <p>LEVEL : Beginner</p>
-                  <button>Register</button>
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="courses-card">
-            <img src={cardImage} alt="card-image" />
-            <div className="courses-infos">
-              <h3 className="courses-card-title">
-                Introduction to Web Development
-              </h3>
-              <div>
-                <span>
-                  <p>By Mahfoud Mehalli</p>
-                  <p>Free</p>
-                </span>
-                <span>
-                  <p>LEVEL : Beginner</p>
+                  <p>LEVEL : Advanced</p>
                   <button>Register</button>
                 </span>
               </div>
